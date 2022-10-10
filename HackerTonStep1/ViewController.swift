e10946169808c93b6551a69b516415cb3ef600c9
stealmh <kmh922@naver.com>
2022-10-11 01:21:24 +0900

    collectionview + pageControl 구현 위치 false시 버튼 클릭하면 label 값이 넘어오게 수정해야함

--- HackerTonStep1/ViewController.swift
+++ HackerTonStep1/ViewController.swift
@@ -15,23 +15,29 @@ class ViewController: UIViewController {
     var paramName: String?
     var currentLocationParamName: String?
     
+    var webImage: [String] = ["batman","captain","ironman","plus.circle"]
+    
+    @IBOutlet weak var myPage: UIPageControl!
     @IBOutlet weak var locationName: UILabel!
+    
     @IBOutlet weak var currentLocationLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        myPage.currentPage = 0
+        myPage.numberOfPages = webImage.count
         
     }
     
-    override func viewWillAppear(_ animated: Bool){
-        if let first = paramName{
-            locationName.text = first
-        }
-        if let second = currentLocationParamName{
-            currentLocationLbl.text = second
-        }
-    }
+//    override func viewWillAppear(_ animated: Bool){
+//        if let first = paramName{
+//            locationName.text = first
+//        }
+//        if let second = currentLocationParamName{
+//            currentLocationLbl.text = second
+//        }
+//    }
     
     
     @IBAction func locationReAllowCheck(_ sender: UIButton) {
@@ -40,11 +46,47 @@ class ViewController: UIViewController {
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        myPage.currentPage = indexPath.row
+        print("idexPath.row : \(indexPath.row)")
+    }
+    
     
     
     
 
 
+}
+
+// MARK: 컬렉션 뷰, 페이지컨트롤 추가
+extension ViewController: UICollectionViewDataSource,UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return webImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCollectionViewCell
+        if indexPath.row == webImage.endIndex - 1 {
+            cell.myImage.image = UIImage(systemName: "plus.circle")
+            cell.myLocationLabel.text = ""
+            cell.myWeatherDescribeLabel.text = "지역 추가하기"
+            cell.tempLabel.text = ""
+            cell.myTempLabel.text = ""
+            cell.humidityLabel.text = ""
+            cell.myHumidityLabel.text = ""
+            cell.hTempLabel.text = ""
+            cell.highTempLabel.text = ""
+            cell.lTempLabel.text = ""
+            cell.lowTempLabel.text = ""
+        }else{
+            cell.myImage.image = UIImage(named: webImage[indexPath.row])
+        }
+        return cell
+        
+    }
+    
+    
 }
 
 // MARK: 위치를 받기 위한 기능들
