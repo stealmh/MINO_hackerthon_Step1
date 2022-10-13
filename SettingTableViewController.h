71f5cfbe7c52ef9cc935d549473d4a20805f675a
stealmh <kmh922@naver.com>
2022-10-13 12:09:05 +0900

    환경설정 연결 / 테이블뷰 섹션 구분 틀 제작

---
+++ SettingTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingTableViewController.h
+//  HackerTonStep1
+//
+//  Created by kimminho on 2022/10/13.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
